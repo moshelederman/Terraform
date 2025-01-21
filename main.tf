809eb1404baafd5a378762712cd04fd41afe6c53
moshelederman <mos9972@gmail.com>
2025-01-21 19:41:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,59 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
-    }
+#
+provider "aws" {
+  region = "us-east-1"
+}
+
+# יצירת Security Group לאפשר גישה ב-HTTP
+resource "aws_security_group" "apache_sg" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
-}
+# יצירת EC2 instance
+resource "aws_instance" "apache_server" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
 
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
-}
+  security_groups = [aws_security_group.apache_sg.name]
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello, Apache is running on AWS!" > /var/www/html/index.html
+            EOF
 
-  ports {
-    internal = 80
-    external = 8000
+  tags = {
+    Name = "Apache-Server"
   }
+}
+
+# הצגת הפלט של כתובת ה-IP הציבורית
+output "public_ip" {
+  value = aws_instance.apache_server.public_ip
+}
+
+# Write public IP to a file using a local-exec provisioner
+resource "null_resource" "write_ip" {
+  provisioner "local-exec" {
+    command = "echo \"${aws_instance.apache_server.public_ip}\" > public_ip.txt"
+  }
+
+  depends_on = [aws_instance.apache_server]
 }
