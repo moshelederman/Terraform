fa69fa8482df4e083fa2aa5f0520759ed62f241e
moshelederman <mos9972@gmail.com>
2025-01-21 20:21:41 +0200

    main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  host = "npipe:////./pipe/docker_engine"
 }
 
 resource "docker_network" "app_network" {
@@ -18,7 +27,7 @@ resource "docker_image" "app_image" {
 
 resource "docker_container" "app" {
   name  = "project-stars"
-  image = docker_image.app_image.latest
+  image = docker_image.app_image.id
 
   ports {
     internal = 5000
@@ -38,14 +47,14 @@ resource "docker_image" "mysql_image" {
 
 resource "docker_container" "db" {
   name  = "docker-gif-db"
-  image = docker_image.mysql_image.latest
+  image = docker_image.mysql_image.id
 
-  environment = {
-    MYSQL_ROOT_PASSWORD = var.mysql_root_password
-    MYSQL_DATABASE      = var.mysql_database
-    MYSQL_USER          = var.mysql_user
-    MYSQL_PASSWORD      = var.mysql_password
-  }
+  env = [
+    "MYSQL_ROOT_PASSWORD=${var.mysql_root_password}",
+    "MYSQL_DATABASE=${var.mysql_database}",
+    "MYSQL_USER=${var.mysql_user}",
+    "MYSQL_PASSWORD=${var.mysql_password}"
+  ]
 
   ports {
     internal = 3306
@@ -54,7 +63,7 @@ resource "docker_container" "db" {
 
   volumes {
     container_path = "/var/lib/mysql"
-    host_path      = docker_volume.db_data.name
+    host_path      = "/c/docker_volumes/db_data"
   }
 
   networks_advanced {
