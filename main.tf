94276c536626ea06d852824457146d9772000e0d
moshelederman <mos9972@gmail.com>
2025-01-21 19:54:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,70 @@
-#
-provider "aws" {
-  region = "us-east-1"
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
 
-# יצירת Security Group לאפשר גישה ב-HTTP
-resource "aws_security_group" "apache_sg" {
-  name        = "allow_http"
-  description = "Allow HTTP traffic"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "docker_network" "app_network" {
+  name = "app_network"
+  driver = "bridge"
 }
 
-# יצירת EC2 instance
-resource "aws_instance" "apache_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
-
-  security_groups = [aws_security_group.apache_sg.name]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install httpd -y
-              systemctl start httpd
-              systemctl enable httpd
-              echo "Hello, Apache is running on AWS!" > /var/www/html/index.html
-            EOF
-
-  tags = {
-    Name = "Apache-Server"
-  }
+resource "docker_volume" "db_data" {
+  name = "db_data"
 }
 
-# הצגת הפלט של כתובת ה-IP הציבורית
-output "public_ip" {
-  value = aws_instance.apache_server.public_ip
+resource "docker_image" "app_image" {
+  name         = "moshelederman/project-stars"
+  keep_locally = false
 }
 
-# Write public IP to a file using a local-exec provisioner
-resource "null_resource" "write_ip" {
-  provisioner "local-exec" {
-    command = "echo \"${aws_instance.apache_server.public_ip}\" > public_ip.txt"
+resource "docker_container" "app" {
+  name  = "project-stars"
+  image = docker_image.app_image.latest
+
+  ports {
+    internal = 5000
+    external = 5000
   }
 
-  depends_on = [aws_instance.apache_server]
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  depends_on = [docker_container.db]
+}
+
+resource "docker_image" "mysql_image" {
+  name = "mysql:8.0"
+}
+
+resource "docker_container" "db" {
+  name  = "docker-gif-db"
+  image = docker_image.mysql_image.latest
+
+  environment = {
+    MYSQL_ROOT_PASSWORD = var.mysql_root_password
+    MYSQL_DATABASE      = var.mysql_database
+    MYSQL_USER          = var.mysql_user
+    MYSQL_PASSWORD      = var.mysql_password
+  }
+
+  ports {
+    internal = 3306
+    external = 3308
+  }
+
+  volumes {
+    container_path = "/var/lib/mysql"
+    host_path      = docker_volume.db_data.name
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  healthcheck {
+    test     = ["CMD", "mysqladmin", "ping", "-h", "127.0.0.1", "-u", var.mysql_user, "-p${var.mysql_password}"]
+    interval = "10s"
+    timeout  = "5s"
+    retries  = 3
+  }
 }
