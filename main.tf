34f6eaeddf586e1764139f7cc68eadda8206d732
moshelederman <mos9972@gmail.com>
2025-01-27 12:25:33 +0200

    added s3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "moshe-terrarorm"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"  # שנה לאזור שלך
 }
